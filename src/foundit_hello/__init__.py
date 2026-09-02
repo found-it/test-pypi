98d4c0b0f985cf4dd4107776e729b8c1186221ec
James Petersen <jpetersenames@gmail.com>
2026-09-01 23:47:59 -0600

    init more
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- src/foundit_hello/__init__.py
+++ src/foundit_hello/__init__.py
@@ -1,5 +1,13 @@
 """A tiny example package demonstrating Python packaging."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 from foundit_hello.greeter import Hello
+
+try:
+    __version__ = version("foundit-hello")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
 
 __all__ = ["Hello"]
